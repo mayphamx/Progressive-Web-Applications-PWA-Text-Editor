2c449f745c55be82714f9d7f6c0e115472da9eef
May <maypham@utexas.edu>
2023-08-28 20:09:14 -0500

    fixed webpack and install js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,6 +3,8 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  
   window.deferredPrompt = event;
 
   butInstall.classList.toggle('hidden', false);
@@ -23,5 +25,6 @@ butInstall.addEventListener('click', async () => {
 });
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+  console.log('install hit') 
   window.deferredPrompt = null;
 });
