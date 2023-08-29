20998526bdd9b7bf57c6998bc339450847a277e8
May <maypham@utexas.edu>
2023-08-28 20:19:08 -0500

    got install pop up to work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,19 +17,16 @@ export const putDb = async (content) => {
 
   console.log('PUT to the database');
   
-  // Create a connection to the database database and version we want to use.
   const contactDb = await openDB('jate', 1);
 
-  // Create a new transaction and specify the database and data privileges.
   const tx = contactDb.transaction('jate', 'readwrite');
 
-  // Open up the desired object store.
+  // Open up the desired object store
   const store = tx.objectStore('jate');
 
-  // Use the .add() method on the store and pass in the content.
+  //store and pass in the content.
   const request = store.put({ id: 1, value: content });
 
-  // Get confirmation of the request.
   const result = await request;
   console.log('Data saved to the database', result);
 };
@@ -39,19 +36,16 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('GET from the database');
   
-  // Create a connection to the database database and version we want to use.
   const contactDb = await openDB('jate', 1);
 
-  // Create a new transaction and specify the database and data privileges.
   const tx = contactDb.transaction('jate', 'readonly');
 
-  // Open up the desired object store.
+  // Open up the desired object store
   const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
+  // get all data in the database.
   const request = store.getAll();
 
-  // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
   return result?.value;
